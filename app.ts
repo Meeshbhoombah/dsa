9c16638bcb89c2ad68753a8a707788f074d69e5e
Meeshbhoombah <rohanmishra2017@gmail.com>
2024-10-23 07:57:11 -0400

    Created github service

--- app.ts
+++ app.ts
@@ -1,15 +1,17 @@
 import * as https from 'https';
 
 
-console.log(process.env.GITHUB_FINE_GRAINED_TOKEN);
-
-
 function pprint(s: string) {
     console.log(JSON.stringify(JSON.parse(s), null, 2))
 }
 
 
+const GITHUB_API = 'https://api.github.com/';
+
+
 /*
+// TODO: update endpoint to `/user-agent`
+
 let httpBinRequest = https.get('https://httpbin.org/headers', (res) => {
 
     res.on('data', (data) => {
@@ -22,37 +24,49 @@ let httpBinRequest = https.get('https://httpbin.org/headers', (res) => {
 */
 
 
-let options = {
-    headers: {
-        'Accept' : 'application/vnd.github+json',
-        'Authorization' : 'Bearer ' + process.env.GITHUB_FINE_GRAINED_TOKEN,
-        'X-GitHub-Api-Version' : '2022-11-28',
-        'User-Agent' : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7)'
-    }
-}
-
-let req = https.get('https://api.github.com/gists', options, (res) => {
-
-    let raw: string = "";
-
-    res.on('data', (data) => {
-        // Looking at the headers of responses from GitHub, the encoding is
-        // `latin1`
-        raw += data.toString('latin1');
-
-    });
-
-    res.on('end', () => {
-  
-        let gists = JSON.parse(raw);
-        for (let gist of gists) {
-            if (gist.files['DSA.md']) {
-                console.log(gist);
+async function get(url: string) {
+    return new Promise<string>((resolve, reject) => {
+        let options = {
+            headers: {
+                'Accept' : 'application/vnd.github+json',
+                'Authorization' : 'Bearer ' + process.env.GITHUB_FINE_GRAINED_TOKEN,
+                'X-GitHub-Api-Version' : '2022-11-28',
+                'User-Agent' : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7)'
             }
         }
-   
-    });
-    
+  
+        https.get(url, options, (res) => {
+            let raw: string = "";
+            res.on('data', (d) => {
+                // GitHub uses `latin1` encoding
+                raw += d.toString('latin1');
+            });
 
-});
+            res.on('end', () => {
+                resolve(raw);
+            });
+
+            res.on('error', (e) => {
+                reject(e) 
+            });
+        });
+    });
+}
+
+
+async function getGists() {
+    let rawGistsString = await get(GITHUB_API + 'gists');
+    let gists = JSON.parse(rawGistsString);
+    return gists;
+}
+
+async function getRawDsaGist() {
+    let gists = await getGists();
+    for (let gist of gists) {
+        if (gist.files['DSA.md']) {
+            let rawDsaGist = await get(gist.files['DSA.md'].raw_url);
+            return rawDsaGist;
+        }
+    }
+}
 
