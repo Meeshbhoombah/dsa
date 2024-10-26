854346c5a09653bb8736db03097aa2f90941c0ca
Meeshbhoombah <rohanmishra2017@gmail.com>
2024-10-26 09:47:30 -0400

    Moved logic to services

--- services/sqlite3.ts
+++ services/sqlite3.ts
@@ -6,17 +6,60 @@ const DB_PATH = process.env.HOME + '/.dsa';
 const DB_NAME = '/database.db';
 
 
-async function createDatabase() {
-    if (!fs.existsSync(DB_PATH)) {
-        fs.mkdir(DB_PATH, (e) => {
+async function createDatabase(path: string, name: string): Promise<Database> {
+    if (!fs.existsSync(path)) {
+        fs.mkdir(path, (e) => {
             throw e;
         });   
     }
 
-    let db: Database = new DatabaseConstructor(DB_PATH + DB_NAME);
-    // Some initialization
+    let db: Database = new DatabaseConstructor(path + name);
     return db;
+}
 
-    console.log(db);
+
+async function createTable(database: Database, query: string) {
+    try {
+        database.prepare(query).run();
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+async function createTables(db: Database) {
+    let tables = [];
+
+    // We use `sha256` as a the file checksum because...
+    // https://security.stackexchange.com/questions/198631/which-hashing-algorithm-shoud-i-use-for-a-safe-file-checksum
+    tables.push(`
+        CREATE TABLE metadatas(
+            date        DATETIME,
+            changed     BOOLEAN, 
+            checksum    BINARY(32)
+        )
+    `);
+
+    tables.push(`
+        CREATE TABLE categories(
+            _id         INT PRIMARY KEY,
+            title       VARCHAR(100),
+            topics      JSON
+        )
+    `);
+
+    tables.push(`
+        CREATE TABLE topics(
+            _id         INT PRIMARY KEY,
+            category    INT,
+            title       VARCHAR(100),
+            days        JSON,
+
+            FOREIGN KEY (category) REFERENCES categories (_id)
+        )
+    `);
+
+    tables.forEach((t) => {
+        createTable(db, t);
+    })
 }
 
