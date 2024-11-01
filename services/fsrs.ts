ae9b127bfc708b20bd984d34ed318b9f6067a4a2
Meeshbhoombah <rohanmishra2017@gmail.com>
2024-11-01 17:13:11 -0400

    Started work on FSRS algorithm

---
+++ services/fsrs.ts
@@ -0,0 +1,47 @@
+import { createCard } from '../repositories/card';
+
+
+interface DefaultParameters = {
+    0: float,
+    1: float,
+    2: float,
+    3: float,
+    4: float,
+    5: float,
+    6: float,
+    7: float,
+    8: float,
+    9: float,
+    10: float,
+    11: float,
+    12: float,
+    13: float,
+    14: float,
+    15: float,
+    16: float,
+    17: float
+}
+
+export async function intitalizeParameters() {
+    return w: DefaultParameters = {
+        0: 
+    }
+}
+
+
+export async function reliability(t: float, S: float) {
+    return (1 + (19 / 81) * (t / S)) ^ -0.5
+}
+
+
+export async function stability(S: float, w: DefaultParameters) {
+    return S * (1 + w.15 * w.16 * e ^ w.8) * 11 - D
+}
+
+
+export async function initial_difficulty(G: float, w: DefaultParameters) {
+    return -w.6 * (G - 3)
+}
+
+export async function difficulty(D: float, )
+
