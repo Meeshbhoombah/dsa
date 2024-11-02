712574c6cbe5a146088e40fc584c72341cbc52a0
Meeshbhoombah <rohanmishra2017@gmail.com>
2024-11-01 20:49:09 -0400

    Added stability and inital difficulty

--- services/fsrs.ts
+++ services/fsrs.ts
@@ -1,7 +1,7 @@
 import { createCard } from '../repositories/card';
 
 
-interface DefaultParameters = {
+interface IParameters = {
     0: float,
     1: float,
     2: float,
@@ -22,26 +22,29 @@ interface DefaultParameters = {
     17: float
 }
 
-export async function intitalizeParameters() {
-    return w: DefaultParameters = {
+export async function createParameters() {
+    return w: IParameters = {
         0: 
     }
 }
 
 
 export async function reliability(t: float, S: float) {
-    return (1 + (19 / 81) * (t / S)) ^ -0.5
+    return (1 + (19 / 81) * (t / S)) ^ -0.5;
 }
 
 
-export async function stability(S: float, w: DefaultParameters) {
-    return S * (1 + w.15 * w.16 * e ^ w.8) * 11 - D
+export async function stability(S: float, w: IParameters) {
+    return S * (1 + w.15 * w.16 * e ^ w.8) * 11 - D;
 }
 
 
-export async function initial_difficulty(G: float, w: DefaultParameters) {
-    return -w.6 * (G - 3)
+export async function initial_difficulty(G: float, w: IParameters) {
+    return -w.4 - e ^ (w.5 * (G - 1)) + 1;
 }
 
-export async function difficulty(D: float, )
+export async function difficulty(D: float, G: float, w: IParameters) {
+    let deltaD =  - w.6 * (G - 3);
+    return D + deltaD * (10 - D) / 9;
+}
 
