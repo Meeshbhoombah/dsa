e7bed7c0c78c225bd0d90a6c61e6193fee86b4fa
Meeshbhoombah <rohanmishra2017@gmail.com>
2024-11-08 14:26:01 -0500

    Started test for fsrs

---
+++ tests/fsrs.ts
@@ -0,0 +1,17 @@
+import {
+    difficulty,
+    initialDifficulty,
+    stability,
+    initialStability,
+    retrievability,
+} from '../services/fsrs';
+
+
+console.log('Topic One: initial rating = good');
+let topicOne = {
+    rating = 'good'
+};
+
+let topicOneInitialDifficulty = initialDifficulty;
+console.log('Initial difficulty: ', topicOneInitialDifficulty);
+
